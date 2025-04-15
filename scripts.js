1879107a9e79c43dc3154038b751f124ad61379c
Gevilee <157779885+gverango@users.noreply.github.com>
2025-04-15 12:44:33 -0700

    Manually cleaned Json data, removed edge case where prev=null and next=null

--- scripts.js
+++ scripts.js
@@ -1,110 +1,4 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
-/*
-
-//Hiiii Commenting this all out 
-const FRESH_PRINCE_URL =
-  "test/bulbasaur.png";
-const CURB_POSTER_URL =
-"test/ivysaur.png"
-const EAST_LOS_HIGH_POSTER_URL =
-"test/venusaur.png"
-// This is an array of strings (TV show titles)
-let titles = [
-  "Bulbasaur",
-  "Ivysaur",
-  "Venusaur",
-  "Charmander"
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
-
-// This function adds cards the page to display the data in the array
-function showCards() {
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = "";
-  const templateCard = document.querySelector(".card");
-
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
-
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
-  }
-}
-
-function editCardContent(card, newTitle, newImageURL) {
-  card.style.display = "block";
-
-  const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
-
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
-
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
-}
-
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!"
-  );
-}
-
-function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
-}
-*/
-window.onload = async function() {
-  const response = await fetch('pokemon_evolution_data.json');
-  const data = await response.json();
-  allCards = data;
-};
-
-/*
+/* // Test Data
 window.onload = function() {
   allCards = [
     {
@@ -169,6 +63,12 @@ window.onload = function() {
 
   ];
 };*/
+
+window.onload = async function() {
+  const response = await fetch('pokemon_evolution_data.json');
+  const data = await response.json();
+  allCards = data;
+};
 
 let drawnCards = [];
 let savedCards = [];
@@ -267,21 +167,6 @@ function removeLastCard() {
   selectedCardIndex = null;   // reset selection
   renderCards();              // update UI
 }
-
-/*
-function removeCard() {
-  if (selectedCardIndex === null) return alert("Select a card to remove!");
-  drawnCards.splice(selectedCardIndex, 1);
-  selectedCardIndex = null;
-  renderCards();
-
-function saveCard() {
-  if (selectedCardIndex === null) return alert("Select a card to save!");
-  const card = drawnCards[selectedCardIndex];
-  if (!savedCards.includes(card)) savedCards.push(card);
-  alert("Card saved!");
-}
-*/ 
 
 // helper function
 function renderCardHTML(card) {
@@ -410,17 +295,6 @@ function displayPulls() {
         }
       });
   
-      chainView.appendChild(row);
-    }
-  
-    // Edge case because the data I pulled from should only be pokemon with an evolution
-    if (standalone.length > 0) {
-      chainView.innerHTML += `<h3>Standalone Pokémon</h3>`;
-      const row = document.createElement("div");
-      row.className = "card-row";
-      standalone.forEach(card => {
-        row.innerHTML += renderCardHTML(card);
-      });
       chainView.appendChild(row);
     }
   }
